15af779e04f440c0c8e036babec259e8a3bf2814
aaryan-makkar <aaryan1010.be23@chitkarauniversity.edu.in>
2026-05-21 12:27:44 +0530

    fix cors for production

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,12 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "https://team-task-manager-brown-psi.vercel.app/"
+    ]
+}));
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
